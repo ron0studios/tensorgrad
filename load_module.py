3479ab3e57b028df39ff5389881c24703684b4ff
Ron0Studios <47331292+ron0studios@users.noreply.github.com>
2023-05-26 20:13:38 +0100

    cleanup

--- load_module.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup, Extension
-
-module = Extension("myModule", sources=["myModule.c"])
-
-setup(name="PackageName", version="1.0", description="package for fib module", 
-      ext_modules = [module])
-
